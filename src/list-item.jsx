830b709f57517adf3300bacf94c39b78280271ba
Robin Fox <Robin Fox>
2015-10-27 14:32:19 -0700

    Added function to list-item that makes sure that when box is checked, firebase makes done: true

--- src/list-item.jsx
+++ src/list-item.jsx
@@ -1,15 +1,25 @@
 var React = require('react');
+var Firebase = require('firebase');
+var rootUrl = 'https://blistering-heat-2046.firebaseio.com/';
 
 module.exports = React.createClass({
   getInitialState: function() {
     return {
-      text: this.props.item.text
+      text: this.props.item.text,
+      done: this.props.item.done
     }
+  },
+  componentWillMount: function() {
+    this.fb = new Firebase(rootUrl + 'items/' + this.props.item.key);
   },
   render: function() {
     return <div className="input-group">
       <span className="input-group-addon">
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={this.state.done}
+          onChange={this.handleDoneChange}
+        />
       </span>
       <input type="text"
         className="form-control"
@@ -21,5 +31,16 @@ module.exports = React.createClass({
         </button>
       </span>
     </div>
+  },
+  handleDoneChange: function(event) {
+    var update = {done: event.target.checked};
+    this.setState(update);
+
+    // This will update any data sitting at this firebase node
+    this.fb.update(update);
   }
 });
+
+// Whenever we have a callback on a form element, or really any element, it will always be called with an event object
+// event.target selects the input
+// event.target.checked to see if the element is checked
