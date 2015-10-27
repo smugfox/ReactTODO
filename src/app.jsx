0417887071bd7d8dcf9822b27c6af23f35acdba9
Robin Fox <Robin Fox>
2015-10-27 13:27:39 -0700

    made list-item component

--- src/app.jsx
+++ src/app.jsx
@@ -27,6 +27,7 @@ var App = React.createClass({
           To-Do List
         </h2>
         <Header itemStore={this.firebaseRefs.items}/>
+        <hr />
         <div className={"content " + (this.state.loaded ? 'loaded' : '')}>
           <List items={this.state.items}/>
         </div>
